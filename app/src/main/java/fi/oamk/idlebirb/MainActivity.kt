ac962175f973a533f93fd52927543fca6b795a36
Kuismai <32294061+Kuismai@users.noreply.github.com>
2022-02-13 16:31:04 +0200

    Tabs working

--- app/src/main/java/fi/oamk/idlebirb/MainActivity.kt
+++ app/src/main/java/fi/oamk/idlebirb/MainActivity.kt
@@ -5,22 +5,53 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pager: ViewPager // creating object of ViewPager
+    private lateinit var tab: TabLayout  // creating object of TabLayout
+    private lateinit var bar: Toolbar    // creating object of ToolBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button_s = findViewById<Button>(R.id.button_s)
+        // set the references of the declared objects above
+        pager = findViewById(R.id.viewPager)
+        tab = findViewById(R.id.tabs)
+        //bar = findViewById(R.id.toolbar)
+
+        // To make our toolbar show the application
+        // we need to give it to the ActionBar
+       // setSupportActionBar(bar)
+
+        // Initializing the ViewPagerAdapter
+        val adapter = MyAdapter(supportFragmentManager)
+
+        // add fragment to the list
+        adapter.addFragment(Home(), "Home")
+        adapter.addFragment(Upgrades(), "Upgrades")
+        adapter.addFragment(Flock(), "Flock")
+
+        // Adding the Adapter to the ViewPager
+        pager.adapter = adapter
+
+        // bind the viewPager with the TabLayout.
+        tab.setupWithViewPager(pager)
+
+       /* val button_s = findViewById<Button>(R.id.button_s)
         button_s.setOnClickListener {
             Toast.makeText (getApplicationContext(), "toast test", Toast.LENGTH_SHORT).show()
             gatherSeeds()
 
-        }
+        }*/
     }
 
 
-    var seeds: Int = 0
+ /*   var seeds: Int = 0
     val seedc by lazy { findViewById<TextView>(R.id.textView1) }
 
 
@@ -28,5 +59,5 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText (getApplicationContext(), "seed got!", Toast.LENGTH_SHORT).show()
         seeds += 1
         seedc.text = seeds.toString()
-    }
+    }*/
 }
